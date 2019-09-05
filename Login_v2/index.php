c6abacb67b1923a831f16bea4de54400da884a72
ongubo <developerworks25@gmail.com>
2019-09-05 21:05:00 +0300

    update

--- Login_v2/index.php
+++ Login_v2/index.php
@@ -32,9 +32,9 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form class="login100-form validate-form" action="https://dashboard.heroku.com">
+				<form class="login100-form validate-form" action="https://upesy.herokuapp.com/">
 					<span class="login100-form-title p-b-26">
-						Upesy ANalytics
+						Upesy Metabase Analytics
 					</span>
 					<span class="login100-form-title p-b-48">
 						<i class="zmdi zmdi-font"></i>
